0e1c820fc3cc10eca851156b08edeea0e501434d
msajdak <maciej@mytimeplan.com>
2017-12-09 15:55:30 +0000

    next messages javascript

--- car/script.js
+++ car/script.js
@@ -14,62 +14,6 @@ function logTrace(text) {
 }
 
 
-//---------------- MESSAGES
-
-
-function sendWebSocket(bytearray){
-    if (webSocket!=undefined){
-        webSocket.send(bytearray.buffer)
-    }else{
-        log("websocket is not opened")
-    }
-}
-
-function sendWebRtc(bytearray){
-    if (openedChanel!=undefined){
-        openedChanel.send(bytearray.buffer)
-    }else{
-        log("webrtc channels is not opened")
-    }
-
-}
-
-
-function getUserMessage(typeId, renevalWhenFailTypeId, receiveDeviceId, messageId, data) {
-
-    if (typeof data === 'string' || data instanceof String){
-        data=Array.from(new TextEncoder("utf-8").encode(data));
-    }
-
-    var bytearray = new Uint8Array(1*2+4*2+data.length);
-    bytearray[0]=typeId;
-    bytearray[1]=renevalWhenFailTypeId;
-    writeShort(bytearray,2,receiveDeviceId);
-    writeShort(bytearray,4,deviceId);
-    writeShort(bytearray,6,messageId);
-    writeShort(bytearray,8,data.length);
-    writeArray(bytearray,10,data);
-
-    logDebug(bytearray);
-
-};
-
-
-
-//----------------Messages end
-
-
-function writeShort(bytearray,pos,num){
-    bytearray[pos]=(num & 0xff00) >> 8;
-    bytearray[pos+1]=(num & 0xff) ;
-}
-function writeArray(bytearray,pos,array){
-    for(var i=0;i<array.length;i++){
-        bytearray[i+pos]=array[i]
-    }
-}
-
-
 
 function getWSUrl() {
     var ret;
